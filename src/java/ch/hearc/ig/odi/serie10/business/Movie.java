d522351cae1d80553c782b371d146088c609d47b
simone.bissolot <simone.bissolot@EGE-BISSOLOTTIS.intra.eiaj.ch>
2015-12-10 08:39:09 +0100

    Suite de la business

--- src/java/ch/hearc/ig/odi/serie10/business/Movie.java
+++ src/java/ch/hearc/ig/odi/serie10/business/Movie.java
@@ -4,9 +4,42 @@ import java.util.*;
 
 public class Movie {
 
-	Collection<Person> people;
+	Map<Integer, Person> people;
 	private long id;
 	private String name;
 	private String producer;
+
+    public Movie(long id, String name, String producer) {
+        this.id = id;
+        this.name = name;
+        this.producer = producer;
+        this.people = new HashMap();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+        
+        
 
 }
--- src/java/ch/hearc/ig/odi/serie10/business/Person.java
+++ src/java/ch/hearc/ig/odi/serie10/business/Person.java
@@ -4,9 +4,41 @@ import java.util.*;
 
 public class Person {
 
-	Collection<Movie> movies;
+	Map<Integer, Movie> movies;
 	private long id;
 	private String firstName;
 	private String lastName;
 
+    public Person(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.movies = new HashMap();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+        
 }
