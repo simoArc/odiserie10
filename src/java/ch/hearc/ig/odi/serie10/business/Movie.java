43dcf13ef545fd92d76b7ccdd55520cff44ca9cd
simone.bissolot <simone.bissolot@EGE-BISSOLOTTIS.intra.eiaj.ch>
2015-12-10 08:28:13 +0100

    Création de l'internalisation

---
+++ src/java/ch/hearc/ig/odi/serie10/business/Movie.java
@@ -0,0 +1,12 @@
+package ch.hearc.ig.odi.serie10.business;
+
+import java.util.*;
+
+public class Movie {
+
+	Collection<Person> people;
+	private long id;
+	private String name;
+	private String producer;
+
+}
---
+++ src/java/ch/hearc/ig/odi/serie10/business/Person.java
@@ -0,0 +1,12 @@
+package ch.hearc.ig.odi.serie10.business;
+
+import java.util.*;
+
+public class Person {
+
+	Collection<Movie> movies;
+	private long id;
+	private String firstName;
+	private String lastName;
+
+}
